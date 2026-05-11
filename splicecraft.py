9f71ea4951a8a8d11fcfbbbe343d60e32fe8645d
ATinyGreenCell <ATinyGreenCell@users.noreply.github.com>
2026-05-11 13:03:11 -0400

    Release v0.7.10.1

--- splicecraft.py
+++ splicecraft.py
@@ -40,7 +40,7 @@ from io import StringIO
 from logging.handlers import RotatingFileHandler
 from pathlib import Path
 
-__version__ = "0.7.10.0"
+__version__ = "0.7.10.1"
 
 # Snapshot the runtime platform string ONCE at module import. On some
 # OSes `platform.platform()` shells out via `subprocess.run` to learn
